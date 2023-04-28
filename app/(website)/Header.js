6330bdaf75a7654bab30496e0c19625fb4b6050e
ChaosCantrip <dev@chaoscantrip.com>
2023-04-28 20:30:44 +0100

    Added header nav

--- app/(website)/Header.js
+++ app/(website)/Header.js
@@ -1,7 +1,15 @@
+import styles from "@/styles/modules/header.module.css";
+import Link from "next/link";
+
 export default function Header() {
     return (
         <header>
             <h1>Header</h1>
+            <nav>
+                <ul><Link href={"/"}>Home</Link></ul>
+                <ul><Link href={"/raids"}>Raids</Link></ul>
+                <ul><Link href={"/dungeons"}>Dungeons</Link></ul>
+            </nav>
         </header>
     )
 }
