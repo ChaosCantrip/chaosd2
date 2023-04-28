ea6c5ce90dd3a6bd2b91574b07a1c4069314cb52
ChaosCantrip <dev@chaoscantrip.com>
2023-04-28 20:58:37 +0100

    Added links to raids and dungeons

--- app/(website)/dungeons/page.js
+++ app/(website)/dungeons/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import layout from "@/styles/modules/layout.module.css";
 
 export default function DungeonsPage() {
@@ -5,6 +6,14 @@ export default function DungeonsPage() {
         <div>
             <h1>Dungeons</h1>
             <p>Page Under Construction</p>
+            <ul>
+                <li><Link href={"/dungeons/SotW"}>Spire of the Watcher</Link></li>
+                <li><Link href={"/dungeons/D"}>Duality</Link></li>
+                <li><Link href={"/dungeons/GoA"}>Grasp of Avarice</Link></li>
+                <li><Link href={"/dungeons/P"}>Prophecy</Link></li>
+                <li><Link href={"/dungeons/PoH"}>Pit of Heresy</Link></li>
+                <li><Link href={"/dungeons/ST"}>Shattered Throne</Link></li>
+            </ul>
         </div>
     )
 }
--- app/(website)/raids/page.js
+++ app/(website)/raids/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import layout from "@/styles/modules/layout.module.css";
 
 export default function Raids() {
@@ -5,6 +7,15 @@ export default function Raids() {
         <div>
             <h1>Raids</h1>
             <p>Page Under Construction</p>
+            <ul>
+                <li><Link href={"/raids/RoN"}>Root of Nightmares</Link></li>
+                <li><Link href={"/raids/KF"}>King's Fall</Link></li>
+                <li><Link href={"/raids/VoG"}>Vow of the Disciple</Link></li>
+                <li><Link href={"/raids/VoG"}>Vault of Glass</Link></li>
+                <li><Link href={"/raids/DSC"}>Deep Stone Crypt</Link></li>
+                <li><Link href={"/raids/GoS"}>Garden of Salvation</Link></li>
+                <li><Link href={"/raids/LW"}>Last Wish</Link></li>
+            </ul>
         </div>
     )
 }
