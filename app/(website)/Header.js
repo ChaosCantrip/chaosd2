772c9bed0d6cbdedcfb175331bd5068128c97452
ChaosCantrip <dev@chaoscantrip.com>
2023-04-28 21:05:11 +0100

    Improved header styling

--- app/(website)/Header.js
+++ app/(website)/Header.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header>
-            <h1>Header</h1>
-            <nav>
+        <header className={styles.header}>
+            <h1 className={styles.title}>destiny.chaoscantrip.com</h1>
+            <nav className={styles.nav}>
                 <ul><Link href={"/"}>Home</Link></ul>
                 <ul><Link href={"/raids"}>Raids</Link></ul>
                 <ul><Link href={"/dungeons"}>Dungeons</Link></ul>
