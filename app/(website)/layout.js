333f91e24bf21953b68d919cd7b9058ec51548a9
ChaosCantrip <dev@chaoscantrip.com>
2023-04-28 19:34:44 +0100

    Added base layout

---
+++ app/(website)/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Destiny 2"
+}
+ 
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
