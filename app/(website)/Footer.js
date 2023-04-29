4dad2088c88b0309e345595cd119f3658a7f3b76
ChaosCantrip <dev@chaoscantrip.com>
2023-04-29 03:51:05 +0100

    Added link to jdonald.co.uk to footer

--- app/(website)/Footer.js
+++ app/(website)/Footer.js
@@ -3,9 +3,9 @@ import styles from "@/styles/modules/footer.module.css";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <p>Website created by ChaosCantrip</p>
+            <p>Website created by <a className={styles.link} href="https://jdonald.co.uk">ChaosCantrip</a></p>
             <p>Not all images are my own, images by other people will be credited.</p>
-            <p><a href={"/legal"}>The Legal Stuff</a></p>
+            <p><a className={styles.link} href={"/legal"}>The Legal Stuff</a></p>
         </footer>
     )
 }
