7170d20d5f8e12bcd4c1a3b404ad91c824ba58a5
ChaosCantrip <dev@chaoscantrip.com>
2023-04-29 03:00:08 +0100

    Added horrible redirects creator

--- next.config.js
+++ next.config.js
@@ -1,4 +1,124 @@
 /** @type {import('next').NextConfig} */
+
+const dictionary = {
+  "raids": {
+    "root_of_nightmares": [
+      "RoN",
+      "ron",
+      "rootofnightmares",
+      "root",
+      "Root"
+    ],
+    "kings_fall": [
+      "KF",
+      "kf",
+      "kingsfall",
+      "kings",
+      "Kings",
+    ],
+    "vow_of_the_disciple": [
+      "VotD",
+      "votd",
+      "vowofthedisciple",
+      "vow",
+      "Vow"
+    ],
+    "vault_of_glass": [
+      "VoG",
+      "vog",
+      "vaultofglass",
+      "vault",
+      "Vault"
+    ],
+    "deep_stone_crypt": [
+      "DSC",
+      "dsc",
+      "deepstonecrypt",
+      "crypt",
+      "Crypt"
+    ],
+    "garden_of_salvation": [
+      "GoS",
+      "gos",
+      "gardenofsalvation",
+      "garden",
+      "Garden"
+    ],
+    "last_wish": [
+      "LW",
+      "lw",
+      "lastwish",
+      "wish",
+      "Wish"
+    ]
+  },
+  "dungeons": {
+    "spire_of_the_watcher": [
+      "SotW",
+      "sotw",
+      "spireofthewatcher",
+      "spire",
+      "Spire"
+    ],
+    "duality": [
+      "D",
+      "d",
+      "Duality",
+      "dual",
+      "Dual"
+    ],
+    "grasp_of_avarice": [
+      "GoA",
+      "goa",
+      "graspofavarice",
+      "grasp",
+      "Grasp"
+    ],
+    "prophecy": [
+      "P",
+      "p",
+      "Prophecy",
+      "proph",
+      "Proph"
+    ],
+    "pit_of_heresy": [
+      "PoH",
+      "poh",
+      "pitofheresy",
+      "pit",
+      "Pit"
+    ],
+    "shattered_throne": [
+      "ST",
+      "st",
+      "shatteredthrone",
+      "throne",
+      "Throne"
+    ]
+  }
+}
+
+function get_redirects() {
+  let redirects = []
+  for (const [type, activities] of Object.entries(dictionary)) {
+    for (const [activity, shortcuts] of Object.entries(activities)) {
+      for (const shortcut of shortcuts) {
+        redirects.push({
+          source: `/${shortcut}`,
+          destination: `/${type}/${activity}`,
+          permanent: true,
+        })
+        redirects.push({
+            source: `/${type}/${shortcut}`,
+            destination: `/${type}/${activity}`,
+            permanent: true,
+        })
+      }
+    }
+  }
+  return redirects
+}
+
 const nextConfig = {
   experimental: {
     appDir: true,
@@ -30,7 +150,7 @@ const nextConfig = {
         destination: "/images/:path*.jpg",
         permanent: true,
       },
-    ]
+    ].concat(get_redirects())
   }
 }
 
