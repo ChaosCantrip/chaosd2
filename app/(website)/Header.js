0eed1e1d96ce0c2568326ad8f3324db666ebabc0
ChaosCantrip <dev@chaoscantrip.com>
2023-04-29 01:44:06 +0100

    Added sub links to header menu

--- app/(website)/Header.js
+++ app/(website)/Header.js
@@ -6,9 +6,32 @@ export default function Header() {
         <header className={styles.header}>
             <h1 className={styles.title}>destiny.chaoscantrip.com</h1>
             <nav className={styles.nav}>
-                <ul><Link href={"/"}>Home</Link></ul>
-                <ul><Link href={"/raids"}>Raids</Link></ul>
-                <ul><Link href={"/dungeons"}>Dungeons</Link></ul>
+                <ul>
+                    <li><Link href={"/"}>Home</Link></li>
+                    <li>
+                        <Link href={"/raids"}>Raids</Link>
+                        <ul>
+                            <li><Link href={"/raids/RoN"}>Root of Nightmares</Link></li>
+                            <li><Link href={"/raids/KF"}>King's Fall</Link></li>
+                            <li><Link href={"/raids/VotD"}>Vow of the Disciple</Link></li>
+                            <li><Link href={"/raids/VoG"}>Vault of Glass</Link></li>
+                            <li><Link href={"/raids/DoS"}>Deep Stone Crypt</Link></li>
+                            <li><Link href={"/raids/GoS"}>Garden of Salvation</Link></li>
+                            <li><Link href={"/raids/LW"}>Last Wish</Link></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <Link href={"/dungeons"}>Dungeons</Link>
+                        <ul>
+                            <li><Link href={"/dungeons/SotW"}>Spire of the Watcher</Link></li>
+                            <li><Link href={"/dungeons/D"}>Duality</Link></li>
+                            <li><Link href={"/dungeons/GoA"}>Grasp of Avarice</Link></li>
+                            <li><Link href={"/dungeons/P"}>Prophecy</Link></li>
+                            <li><Link href={"/dungeons/PoH"}>Pit of Heresy</Link></li>
+                            <li><Link href={"/dungeons/SH"}>Shattered Throne</Link></li>
+                        </ul>
+                    </li>
+                </ul>
             </nav>
         </header>
     )
