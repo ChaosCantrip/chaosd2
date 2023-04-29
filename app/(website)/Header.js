ad8b0f758f535a312df56ebc46eba202f9d45198
ChaosCantrip <dev@chaoscantrip.com>
2023-04-29 13:00:57 +0100

    Changed a to Link in header

--- app/(website)/Header.js
+++ app/(website)/Header.js
@@ -1,40 +1,41 @@
 import styles from "@/styles/modules/header.module.css";
 import Image from "next/image";
+import Link from "next/link";
 import logo from "@/public/images/logo.png";
 
 export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.content}>
-                <a href={"/"} className={styles.logo_link}>
+                <Link href={"/"} className={styles.logo_link}>
                     <Image src={logo} alt={""} className={styles.logo}/>
-                </a>
+                </Link>
                 <nav className={styles.nav}>
-                    <div><a className={styles.link} href={"/"}>Home</a></div>
+                    <div><Link className={styles.link} href={"/"}>Home</Link></div>
                     <div>
-                        <a className={styles.link} href={"/raids"}>Raids</a>
+                        <Link className={styles.link} href={"/raids"}>Raids</Link>
                         <div className={styles.sub_menu}>
-                            <a href={"/raids/root_of_nightmares"}>Root of Nightmares</a>
-                            <a href={"/raids/kings_fall"}>King's Fall</a>
-                            <a href={"/raids/vow_of_the_disciple"}>Vow of the Disciple</a>
-                            <a href={"/raids/vault_of_glass"}>Vault of Glass</a>
-                            <a href={"/raids/deep_stone_crypt"}>Deep Stone Crypt</a>
-                            <a href={"/raids/garden_of_salvation"}>Garden of Salvation</a>
-                            <a href={"/raids/last_wish"}>Last Wish</a>
+                            <Link href={"/raids/root_of_nightmares"}>Root of Nightmares</Link>
+                            <Link href={"/raids/kings_fall"}>King's Fall</Link>
+                            <Link href={"/raids/vow_of_the_disciple"}>Vow of the Disciple</Link>
+                            <Link href={"/raids/vault_of_glass"}>Vault of Glass</Link>
+                            <Link href={"/raids/deep_stone_crypt"}>Deep Stone Crypt</Link>
+                            <Link href={"/raids/garden_of_salvation"}>Garden of Salvation</Link>
+                            <Link href={"/raids/last_wish"}>Last Wish</Link>
                         </div>
                     </div>
                     <div>
-                        <a className={styles.link} href={"/dungeons"}>Dungeons</a>
+                        <Link className={styles.link} href={"/dungeons"}>Dungeons</Link>
                         <div className={styles.sub_menu}>
-                            <a href={"/dungeons/spire_of_the_watcher"}>Spire of the Watcher</a>
-                            <a href={"/dungeons/duality"}>Duality</a>
-                            <a href={"/dungeons/grasp_of_avarice"}>Grasp of Avarice</a>
-                            <a href={"/dungeons/prophecy"}>Prophecy</a>
-                            <a href={"/dungeons/pit_of_heresy"}>Pit of Heresy</a>
-                            <a href={"/dungeons/shattered_throne"}>Shattered Throne</a>
+                            <Link href={"/dungeons/spire_of_the_watcher"}>Spire of the Watcher</Link>
+                            <Link href={"/dungeons/duality"}>Duality</Link>
+                            <Link href={"/dungeons/grasp_of_avarice"}>Grasp of Avarice</Link>
+                            <Link href={"/dungeons/prophecy"}>Prophecy</Link>
+                            <Link href={"/dungeons/pit_of_heresy"}>Pit of Heresy</Link>
+                            <Link href={"/dungeons/shattered_throne"}>Shattered Throne</Link>
                         </div>
                     </div>
-                    <div><a href={"/reference"}>Reference</a></div>
+                    <div><Link href={"/reference"}>Reference</Link></div>
                 </nav>
             </div>
         </header>
