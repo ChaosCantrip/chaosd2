7fa41193b748b3e69217d78c19ce41824b4fcc1d
ChaosCantrip <dev@chaoscantrip.com>
2023-04-29 13:04:10 +0100

    Added not-found.js

---
+++ app/(website)/not-found.js
@@ -0,0 +1,9 @@
+import layout from "@/styles/modules/layout.module.css";
+
+export default function NotFound() {
+    return (
+        <div className={layout.page}>
+            <h1 className={layout.page_title}>Page not found</h1>
+        </div>
+    )
+}
