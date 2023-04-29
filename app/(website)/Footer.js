d2eef9b802d8737b84158c1ba86018f99601745c
ChaosCantrip <dev@chaoscantrip.com>
2023-04-29 03:19:10 +0100

    Updated footer and added /legal

--- app/(website)/Footer.js
+++ app/(website)/Footer.js
@@ -1,7 +1,11 @@
+import styles from "@/styles/modules/footer.module.css";
+
 export default function Footer() {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer className={styles.footer}>
+            <p>Website created by ChaosCantrip</p>
+            <p>Not all images are my own, images by other people will be credited.</p>
+            <p><a href={"/legal"}>The Legal Stuff</a></p>
         </footer>
     )
 }
---
+++ app/(website)/legal/page.js
@@ -0,0 +1,15 @@
+export default function LegalPage() {
+    return (
+        <div>
+            <h1>The Legal Stuff</h1>
+            <p>idk much about legal stuff I'm just some dude</p>
+            <p>I'm not associated with Destiny 2 or Bungie in any way</p>
+            <p>I'm just a fan of the game that wanted to put together an easy way to find maps for encounters and stuff</p>
+            <p>The main purpose of the site is just the quick links system</p>
+            <p>Please don't sue me lmfao</p>
+            <p>If you have a problem with something on the page, please just contact me at <a href="mailto:dev@chaoscantrip.com">dev@chaoscantrip.com</a>, and I'll do my best to accomodate.</p>
+            <p>I'm in the process of remaking these maps on my own, but in the meantime I just put up the ones everyone was using</p>
+            <p>Thanks for reading!</p>
+        </div>
+    )
+}
