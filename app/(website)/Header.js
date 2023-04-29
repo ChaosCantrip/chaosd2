c938fb65c63a92c0e8eac49562dc103ded06e840
ChaosCantrip <dev@chaoscantrip.com>
2023-04-29 01:56:00 +0100

    Implemented layout of header nav

--- app/(website)/Header.js
+++ app/(website)/Header.js
@@ -4,11 +4,12 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <h1 className={styles.title}>destiny.chaoscantrip.com</h1>
-            <nav className={styles.nav}>
-                <div><Link href={"/"}>Home</Link></div>
-                <div className={styles.menu}>
-                        <Link href={"/raids"}>Raids</Link>
+            <div className={styles.content}>
+                <h1 className={styles.title}>destiny.chaoscantrip.com</h1>
+                <nav className={styles.nav}>
+                    <div><Link className={styles.link} href={"/"}>Home</Link></div>
+                    <div>
+                        <Link className={styles.link} href={"/raids"}>Raids</Link>
                         <div className={styles.sub_menu}>
                             <Link href={"/raids/RoN"}>Root of Nightmares</Link>
                             <Link href={"/raids/KF"}>King's Fall</Link>
@@ -18,19 +19,20 @@ export default function Header() {
                             <Link href={"/raids/GoS"}>Garden of Salvation</Link>
                             <Link href={"/raids/LW"}>Last Wish</Link>
                         </div>
-                </div>
-                <div className={styles.menu}>
-                    <Link href={"/dungeons"}>Dungeons</Link>
-                    <div className={styles.sub_menu}>
-                        <Link href={"/dungeons/SotW"}>Spire of the Watcher</Link>
-                        <Link href={"/dungeons/D"}>Duality</Link>
-                        <Link href={"/dungeons/GoA"}>Grasp of Avarice</Link>
-                        <Link href={"/dungeons/P"}>Prophecy</Link>
-                        <Link href={"/dungeons/PoH"}>Pit of Heresy</Link>
-                        <Link href={"/dungeons/SH"}>Shattered Throne</Link>
                     </div>
-                </div>
-            </nav>
+                    <div>
+                        <Link className={styles.link} href={"/dungeons"}>Dungeons</Link>
+                        <div className={styles.sub_menu}>
+                            <Link href={"/dungeons/SotW"}>Spire of the Watcher</Link>
+                            <Link href={"/dungeons/D"}>Duality</Link>
+                            <Link href={"/dungeons/GoA"}>Grasp of Avarice</Link>
+                            <Link href={"/dungeons/P"}>Prophecy</Link>
+                            <Link href={"/dungeons/PoH"}>Pit of Heresy</Link>
+                            <Link href={"/dungeons/SH"}>Shattered Throne</Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </header>
     )
 }
