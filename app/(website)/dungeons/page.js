44ad217bd69575d87974d27b648c39a9b0d38fe1
ChaosCantrip <dev@chaoscantrip.com>
2023-05-02 22:54:09 +0100

    Added raid and dungeon grids

--- app/(website)/dungeons/page.js
+++ app/(website)/dungeons/page.js
@@ -4,25 +4,43 @@ import Title from "@/lib/components/Title";
 import { BungieIcons } from "@/lib/BungieIcons";
 import BackgroundWrapper from "@/lib/components/BackgroundWrapper";
 import backgrounds from "@/styles/modules/backgrounds.module.css";
+import ActivityGridItem from "@/lib/components/ActivityGridItem";
+
+import SpireOfTheWatcher from "@/public/images/backgrounds/spire.jpg";
+import Duality from "@/public/images/backgrounds/duality.jpg";
+import GraspOfAvarice from "@/public/images/backgrounds/grasp.jpg";
+import Prophecy from "@/public/images/backgrounds/prophecy.jpg";
+import PitOfHeresy from "@/public/images/backgrounds/pit.jpg";
+import ShatteredThrone from "@/public/images/backgrounds/st.jpg";
 
 export const metadata = {
     "title": "Dungeons",
 }
+
+/*
+Spire of the Watcher
+Duality
+Grasp of Avarice
+Prophecy
+Pit of Heresy
+Shattered Throne
+ */
 
 export default function DungeonsPage() {
     return (
         <BackgroundWrapper background={backgrounds.dungeon}>
             <div className={layout.page}>
                 <Title title={"Dungeons"} icon_url={BungieIcons.dungeon_icon} show_back_button={false}/>
-                <p>Page Under Construction</p>
-                <ul>
-                    <li><Link href={"/dungeons/spire_of_the_watcher"}>Spire of the Watcher</Link></li>
-                    <li><Link href={"/dungeons/duality"}>Duality</Link></li>
-                    <li><Link href={"/dungeons/grasp_of_avarice"}>Grasp of Avarice</Link></li>
-                    <li><Link href={"/dungeons/prophecy"}>Prophecy</Link></li>
-                    <li><Link href={"/dungeons/pit_of_heresy"}>Pit of Heresy</Link></li>
-                    <li><Link href={"/dungeons/shattered_throne"}>Shattered Throne</Link></li>
-                </ul>
+                <div className={layout.page_content}>
+                    <div className={layout.activity_grid}>
+                        <ActivityGridItem href={"/dungeons/spire_of_the_watcher"} name={"Spire of the Watcher"} icon={BungieIcons.dungeons.sotw} banner={SpireOfTheWatcher}/>
+                        <ActivityGridItem href={"/dungeons/duality"} name={"Duality"} icon={BungieIcons.dungeons.duality} banner={Duality}/>
+                        <ActivityGridItem href={"/dungeons/grasp_of_avarice"} name={"Grasp of Avarice"} icon={BungieIcons.dungeons.goa} banner={GraspOfAvarice}/>
+                        <ActivityGridItem href={"/dungeons/prophecy"} name={"Prophecy"} icon={BungieIcons.dungeons.prophecy} banner={Prophecy}/>
+                        <ActivityGridItem href={"/dungeons/pit_of_heresy"} name={"Pit of Heresy"} icon={BungieIcons.dungeons.poh} banner={PitOfHeresy}/>
+                        <ActivityGridItem href={"/dungeons/shattered_throne"} name={"Shattered Throne"} icon={BungieIcons.dungeons.st} banner={ShatteredThrone}/>
+                    </div>
+                </div>
             </div>
         </BackgroundWrapper>
     )
--- app/(website)/raids/page.js
+++ app/(website)/raids/page.js
@@ -5,26 +5,46 @@ import layout from "@/styles/modules/layout.module.css";
 import {BungieIcons} from "@/lib/BungieIcons";
 import BackgroundWrapper from "@/lib/components/BackgroundWrapper";
 import backgrounds from "@/styles/modules/backgrounds.module.css";
+import ActivityGridItem from "@/lib/components/ActivityGridItem";
+
+import RootOfNightmares from "@/public/images/backgrounds/root.jpg";
+import KingsFall from "@/public/images/backgrounds/kf.jpg";
+import VowOfTheDisciple from "@/public/images/backgrounds/vow.jpg";
+import VaultOfGlass from "@/public/images/backgrounds/vog.jpg";
+import DeepStoneCrypt from "@/public/images/backgrounds/dsc.jpg";
+import GardenOfSalvation from "@/public/images/backgrounds/garden.jpg";
+import LastWish from "@/public/images/backgrounds/last_wish.jpg";
 
 export const metadata = {
     "title": "Raids",
 }
+
+/*
+Root of Nightmares
+King's Fall
+Vow of the Disciple
+Vault of Glass
+Deep Stone Crypt
+Garden of Salvation
+Last Wish
+ */
 
 export default function Raids() {
     return (
         <BackgroundWrapper background={backgrounds.raid}>
             <div className={layout.page}>
                 <Title title={"Raids"} icon_url={BungieIcons.raid_icon} show_back_button={false}/>
-                <p>Page Under Construction</p>
-                <ul>
-                    <li><Link href={"/raids/root_of_nightmares"}>Root of Nightmares</Link></li>
-                    <li><Link href={"/raids/kings_fall"}>King's Fall</Link></li>
-                    <li><Link href={"/raids/vow_of_the_disciple"}>Vow of the Disciple</Link></li>
-                    <li><Link href={"/raids/vault_of_glass"}>Vault of Glass</Link></li>
-                    <li><Link href={"/raids/deep_stone_crypt"}>Deep Stone Crypt</Link></li>
-                    <li><Link href={"/raids/garden_of_salvation"}>Garden of Salvation</Link></li>
-                    <li><Link href={"/raids/last_wish"}>Last Wish</Link></li>
-                </ul>
+                <div className={layout.page_content}>
+                    <div className={layout.activity_grid}>
+                        <ActivityGridItem href={"/raids/root_of_nightmares"} name={"Root of Nightmares"} icon={BungieIcons.raids.ron} banner={RootOfNightmares}/>
+                        <ActivityGridItem href={"/raids/kings_fall"} name={"King's Fall"} icon={BungieIcons.raids.kf} banner={KingsFall}/>
+                        <ActivityGridItem href={"/raids/vow_of_the_disciple"} name={"Vow of the Disciple"} icon={BungieIcons.raids.votd} banner={VowOfTheDisciple}/>
+                        <ActivityGridItem href={"/raids/vault_of_glass"} name={"Vault of Glass"} icon={BungieIcons.raids.vog} banner={VaultOfGlass}/>
+                        <ActivityGridItem href={"/raids/deep_stone_crypt"} name={"Deep Stone Crypt"} icon={BungieIcons.raids.dsc} banner={DeepStoneCrypt}/>
+                        <ActivityGridItem href={"/raids/garden_of_salvation"} name={"Garden of Salvation"} icon={BungieIcons.raids.gos} banner={GardenOfSalvation}/>
+                        <ActivityGridItem href={"/raids/last_wish"} name={"Last Wish"} icon={BungieIcons.raids.lw} banner={LastWish}/>
+                    </div>
+                </div>
             </div>
         </BackgroundWrapper>
     )
