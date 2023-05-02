e3fe4dde989db8b88e4f231077f89c7368605e54
ChaosCantrip <dev@chaoscantrip.com>
2023-05-02 16:35:18 +0100

    Added twitter summary_large_image

--- app/(website)/dungeons/duality/loot/page.js
+++ app/(website)/dungeons/duality/loot/page.js
@@ -2,6 +2,14 @@
 import EncounterMap from "@/public/images/dungeons/duality/loot.png";
 import EncounterPage from "@/lib/components/EncounterPage";
 
+export const metadata = {
+    name: "Duality Loot Table | chaosd2",
+    description: "Loot table for the Duality raid.",
+    twitter: {
+        card: "summary_large_image",
+    }
+}
+
 export default function LootPage() {
     return (
         <EncounterPage
