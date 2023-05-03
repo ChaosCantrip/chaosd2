b655ed14ea29726cfd8717b0bc55939efae68226
ChaosCantrip <dev@chaoscantrip.com>
2023-05-03 18:32:45 +0100

    Fixed Shattered Throne layout

--- app/(website)/dungeons/shattered_throne/layout.js
+++ app/(website)/dungeons/shattered_throne/layout.js
@@ -4,13 +4,13 @@ import BackgroundWrapper from "@/lib/components/BackgroundWrapper";
 import backgrounds from "@/styles/modules/backgrounds.module.css";
 
 export const metadata = {
-    "title": "Spire of the Watcher",
+    "title": "Shattered Throne",
 }
 
 export default function ThroneLayout({children}) {
     return (
         <BackgroundWrapper background={backgrounds.st}>
-            <ActivityLayout title={"Spire of the Watcher"} icon_url={BungieIcons.dungeons.st}>
+            <ActivityLayout title={"Shattered Throne"} icon_url={BungieIcons.dungeons.st}>
                 {children}
             </ActivityLayout>
         </BackgroundWrapper>
