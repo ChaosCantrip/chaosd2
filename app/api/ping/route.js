86982f38c4ca89407dd00398fbe174da983fd2ce
ChaosCantrip <dev@chaoscantrip.com>
2023-05-10 07:10:30 +0100

    Removed api (unused)

--- app/api/ping/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request) {
-    console.log(request.headers);
-    return NextResponse.json({
-        "message": "pong"
-    })
-}
