80bec8ef3986aef0c4cd7a5d01809bad479eb68d
ChaosCantrip <dev@chaoscantrip.com>
2023-05-10 07:42:18 +0100

    Added TODO

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,8 @@
 import layout from "@src/styles/modules/layout.module.css";
 import Title from "@components/Title";
 
+/* TODO: Make this page */
+
 export default function NotFound() {
     return (
         <div className={layout.page}>
