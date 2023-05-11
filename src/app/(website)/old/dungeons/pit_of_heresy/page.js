7bf4670d4151bc2627b562a68477f9f14214842f
ChaosCantrip <dev@chaoscantrip.com>
2023-05-11 11:02:00 +0100

    Deleted old

--- src/app/(website)/old/dungeons/pit_of_heresy/page.js
+++
@@ -1,9 +0,0 @@
-import layout from "@styles/modules/layout.module.css";
-
-export default function PitOfHeresy() {
-    return (
-        <div className={layout.page_content}>
-            <p>Page Under Construction</p>
-        </div>
-    )
-}
