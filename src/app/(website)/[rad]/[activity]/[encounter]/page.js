d382cd148f72c5e6c4269521b4ab56ab457b311f
ChaosCantrip <dev@chaoscantrip.com>
2023-05-11 10:23:15 +0100

    Updated EncounterPage

--- src/app/(website)/[rad]/[activity]/[encounter]/page.js
+++ src/app/(website)/[rad]/[activity]/[encounter]/page.js
@@ -2,15 +2,21 @@ import layout from "@styles/modules/layout.module.css";
 import Image from "next/image";
 import QuickLink from "@components/QuickLink";
 import Link from "next/link";
+import {PathConfig} from "@components/PathConfig";
+import {image_sources} from "@components/ImageImports";
 
 export default function EncounterPage({ params }) {
+    const { rad, activity, encounter } = params;
+    const data = PathConfig[rad][activity].encounters[encounter];
+    const image = image_sources[rad][activity][encounter];
+    const { name, ql } = data;
     return (
         <div className={layout.encounter_page}>
-            <h2 className={layout.page_subtitle}>{encounter_name}</h2>
+            <h2 className={layout.page_subtitle}>{name}</h2>
             <div className={layout.page_content}>
-                <QuickLink href={image_ql}/>
-                <Link href={image_ql + "/i"}>
-                    <Image className={layout.full_image} src={map} alt={"Image Link"} placeholder="blur"/>
+                <QuickLink href={ql}/>
+                <Link href={ql + "/i"}>
+                    <Image className={layout.full_image} src={image} alt={"Image Link"} placeholder="blur"/>
                 </Link>
             </div>
         </div>
