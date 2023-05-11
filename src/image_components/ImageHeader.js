2913dc425347a6a5cbda7f69835a998dc548e1ee
ChaosCantrip <dev@chaoscantrip.com>
2023-05-11 18:08:55 +0100

    Updated ImageHeader to allow hiding of icon

--- src/image_components/ImageHeader.js
+++ src/image_components/ImageHeader.js
@@ -3,7 +3,9 @@ import styles from "@image_components/ImageHeader.module.css";
 export default function ImageHeader({ icon, title, subtitle, href }) {
     return (
         <div className={styles.header}>
-            <img className={styles.logo} src={icon} alt={""}/>
+            {
+                icon !== undefined ? <img className={styles.logo} src={icon} alt={""}/> : null
+            }
             <div className={styles.header_text}>
                 <h1 className={styles.title}>{title}</h1>
                 <p className={styles.subtitle}>{subtitle}</p>
