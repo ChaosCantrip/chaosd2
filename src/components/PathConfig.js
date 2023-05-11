bf9b48da4afc9e11f8333a89121157bcd5b0d942
ChaosCantrip <dev@chaoscantrip.com>
2023-05-11 09:24:25 +0100

    Added PathConfig

---
+++ src/components/PathConfig.js
@@ -0,0 +1,379 @@
+export const PathConfig = {
+    raids: {
+        root_of_nightmares: {
+            name: "Root of Nightmares",
+            encounters: {
+                1: {
+                    name: "Cataclysm",
+                    ql: "/root/1",
+                },
+                2: {
+                    name: "Scission",
+                    ql: "/root/2",
+                },
+                3: {
+                    name: "Macrocosm",
+                    ql: "/root/3",
+                },
+                4: {
+                    name: "Nezarec, Final God of Pain",
+                    ql: "/root/4",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/root/loot",
+                },
+                rb: {
+                    name: "Red Border Chest",
+                    ql: "/root/rb",
+                }
+            }
+        },
+        kings_fall: {
+            name: "King's Fall",
+            encounters: {
+                0: {
+                    name: "Opening the Door",
+                    ql: "/kf/0",
+                },
+                1: {
+                    name: "Totems of Annihilation",
+                    ql: "/kf/1",
+                },
+                2: {
+                    name: "Warpriest",
+                    ql: "/kf/2",
+                },
+                3: {
+                    name: "Golgoroth",
+                    ql: "/kf/3",
+                },
+                4: {
+                    name: "Daughters of Oryx",
+                    ql: "/kf/4",
+                },
+                5: {
+                    name: "Oryx, the Taken King",
+                    ql: "/kf/5",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/kf/loot",
+                },
+                rb: {
+                    name: "Red Border Chest",
+                    ql: "/kf/rb",
+                },
+                maze: {
+                    name: "Golgoroth's Cellar",
+                    ql: "/kf/maze",
+                }
+            }
+        },
+        vow_of_the_disciple: {
+            name: "Vow of the Disciple",
+            encounters: {
+                0: {
+                    name: "Payload",
+                    ql: "/vow/0",
+                },
+                1: {
+                    name: "Acquisition",
+                    ql: "/vow/1",
+                },
+                2: {
+                    name: "Caretaker",
+                    ql: "/vow/2",
+                },
+                3: {
+                    name: "Exhibition",
+                    ql: "/vow/3",
+                },
+                4: {
+                    name: "Rhulk, Disciple of the Witness",
+                    ql: "/vow/4",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/vow/loot",
+                },
+                rb: {
+                    name: "Red Border Chest",
+                    ql: "/vow/rb",
+                },
+                symbols: {
+                    name: "Symbols",
+                    ql: "/vow/symbols",
+                }
+            }
+        },
+        vault_of_glass: {
+            name: "Vault of Glass",
+            encounters: {
+                0: {
+                    name: "Form the Spire",
+                    ql: "/vog/0",
+                },
+                1: {
+                    name: "Confluxes",
+                    ql: "/vog/1",
+                },
+                2: {
+                    name: "Oracles",
+                    ql: "/vog/2",
+                },
+                3: {
+                    name: "Templar",
+                    ql: "/vog/3",
+                },
+                4: {
+                    name: "Gatekeepers",
+                    ql: "/vog/4",
+                },
+                5: {
+                    name: "Atheon, Time's Conflux",
+                    ql: "/vog/5",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/vog/loot",
+                },
+                maze: {
+                    name: "Gorgon's Labyrinth",
+                    ql: "/vog/maze",
+                }
+            }
+        },
+        deep_stone_crypt: {
+            name: "Deep Stone Crypt",
+            encounters: {
+                1: {
+                    name: "Crypt Security",
+                    ql: "/dsc/1",
+                },
+                2: {
+                    name: "Atraks-1",
+                    ql: "/dsc/2",
+                },
+                3: {
+                    name: "Nuclear Descent",
+                    ql: "/dsc/3",
+                },
+                4: {
+                    name: "Taniks, the Abomination",
+                    ql: "/dsc/4",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/dsc/loot",
+                }
+            }
+        },
+        garden_of_salvation: {
+            name: "Garden of Salvation",
+            encounters: {
+                1: {
+                    name: "Embrace",
+                    ql: "/gos/1",
+                },
+                2: {
+                    name: "Undergrowth",
+                    ql: "/gos/2",
+                },
+                3: {
+                    name: "Defeat the Consecrated Mind",
+                    ql: "/gos/3",
+                },
+                4: {
+                    name: "Consecrated Mind, Sol Inherent",
+                    ql: "/gos/4",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/gos/loot",
+                }
+            }
+        },
+        last_wish: {
+            name: "Last Wish",
+            encounters: {
+                1: {
+                    name: "Kalli, the Corrupted",
+                    ql: "/lw/1",
+                },
+                2: {
+                    name: "Shuro Chi, the Corrupted",
+                    ql: "/lw/2",
+                },
+                3: {
+                    name: "Morgeth, the Spirekeeper",
+                    ql: "/lw/3",
+                },
+                4: {
+                    name: "The Vault",
+                    ql: "/lw/4",
+                },
+                5: {
+                    name: "Riven of a Thousand Voices",
+                    ql: "/lw/5",
+                },
+                6: {
+                    name: "Queenswalk",
+                    ql: "/lw/6",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/lw/loot",
+                },
+                symbols: {
+                    name: "Symbols",
+                    ql: "/lw/symbols",
+                }
+            }
+        }
+    },
+    dungeons: {
+        spire_of_the_watcher: {
+            name: "Spire of the Watcher",
+            encounters: {
+                0: {
+                    name: "Opening - Power Cables",
+                    ql: "/spire/0",
+                },
+                1: {
+                    name: "Ascend the Spire",
+                    ql: "/spire/1",
+                },
+                2: {
+                    name: "Silence the Spire",
+                    ql: "/spire/2",
+                },
+                3: {
+                    name: "Persys, Primordial Ruin",
+                    ql: "/spire/3",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/spire/loot",
+                }
+            }
+        },
+        duality: {
+            name: "Duality",
+            encounters: {
+                1: {
+                    name: "Nightmare of Gahlran",
+                    ql: "/duality/1",
+                },
+                2: {
+                    name: "Opening the Vault",
+                    ql: "/duality/2",
+                },
+                3: {
+                    name: "Nightmare of Caiatl",
+                    ql: "/duality/3",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/duality/loot",
+                },
+                statues: {
+                    name: "Statues Puzzle",
+                    ql: "/duality/statues",
+                }
+            }
+        },
+        grasp_of_avarice: {
+            name: "Grasp of Avarice",
+            encounters: {
+                1: {
+                    name: "Phry'zia, the Insatiable",
+                    ql: "/grasp/1",
+                },
+                2: {
+                    name: "Fallen Shield",
+                    ql: "/grasp/2",
+                },
+                3: {
+                    name: "Captain Avarokk, the Covetous",
+                    ql: "/grasp/3",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/grasp/loot",
+                }
+            }
+        },
+        prophecy: {
+            name: "Prophecy",
+            encounters: {
+                1: {
+                    name: "Phalanx Echo",
+                    ql: "/prophecy/1",
+                },
+                2: {
+                    name: "Hexahedron",
+                    ql: "/prophecy/2",
+                },
+                3: {
+                    name: "Kell Echo",
+                    ql: "/prophecy/3",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/prophecy/loot",
+                }
+            }
+        },
+        pit_of_heresy: {
+            name: "Pit of Heresy",
+            encounters: {
+                1: {
+                    name: "The Necropolis",
+                    ql: "/pit/1",
+                },
+                2: {
+                    name: "Tunnels of Despair",
+                    ql: "/pit/2",
+                },
+                3: {
+                    name: "Chamber of Suffering",
+                    ql: "/pit/3",
+                },
+                4: {
+                    name: "The Harrow",
+                    ql: "/pit/4",
+                },
+                5: {
+                    name: "Zulmak, Instrument of Torment",
+                    ql: "/pit/5",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/pit/loot",
+                }
+            }
+        },
+        shattered_throne: {
+            name: "Shattered Throne",
+            encounters: {
+                1: {
+                    name: "Search for Safe Passage",
+                    ql: "/st/1",
+                },
+                2: {
+                    name: "Vorgeth, the Boundless Hunger",
+                    ql: "/st/2",
+                },
+                3: {
+                    name: "Dul Incaru, the Eternal Return",
+                    ql: "/st/3",
+                },
+                loot: {
+                    name: "Loot Table",
+                    ql: "/st/loot",
+                }
+            }
+        }
+    }
+}
