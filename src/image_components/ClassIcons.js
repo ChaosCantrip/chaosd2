db4d894b3123457f86627df5d1fdb02540eaee41
ChaosCantrip <dev@chaoscantrip.com>
2023-05-24 00:25:11 +0100

    Added ClassIcons

---
+++ src/image_components/ClassIcons.js
@@ -0,0 +1,5 @@
+export const ClassIcons = {
+    Titan: "https://www.bungie.net/common/destiny2_content/icons/46a19ddd00d0f6ca822230943103b54a.png",
+    Warlock: "https://www.bungie.net/common/destiny2_content/icons/e4006d9a8fe167bd7e83193d7601c89a.png",
+    Hunter: "https://www.bungie.net/common/destiny2_content/icons/05e32a388d9a65a0ef59b2193eee2db4.png"
+}
