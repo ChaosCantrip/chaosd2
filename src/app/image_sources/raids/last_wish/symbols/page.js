2358dff227db4b89cc1cb498830498f2adf24ca5
ChaosCantrip <dev@chaoscantrip.com>
2023-05-24 00:55:15 +0100

    Added /lw/symbols

--- src/app/image_sources/raids/last_wish/symbols/page.js
+++ src/app/image_sources/raids/last_wish/symbols/page.js
@@ -3,9 +3,27 @@ import ImageHeader from "@image_components/ImageHeader";
 import Image from "next/image";
 import Background from "@backgrounds/last_wish.jpg";
 import {BungieIcons} from "@components/BungieIcons";
-import ImageUnderConstruction from "@image_components/ImageUnderConstruction";
+import {LWSymbols} from "@image_components/LWSymbols";
+import custom from "./custom.module.css"
 
-/* TODO: This is a placeholder page. */
+/*
+    branch_bird: Branch_Bird,
+    circle_fish: Circle_Fish,
+    curl_fish: Curl_Fish,
+    down_bird: Down_Bird,
+    eight_snake: Eight_Snake,
+    fire_left_dragon: Fire_Left_Dragon,
+    fire_right_dragon: Fire_Right_Dragon,
+    flying_bird: Flying_Bird,
+    infinity_snake: Infinity_Snake,
+    left_bird: Left_Bird,
+    left_fish: Left_Fish,
+    spear_hold_dragon: Spear_Hold_Dragon,
+    spear_neck_dragon: Spear_Neck_Dragon,
+    two_snake: Two_Snake,
+    up_fish: Up_Fish,
+    uwu_snake: Uwu_Snake,
+ */
 
 export const metadata = {
     other: {
@@ -14,16 +32,42 @@ export const metadata = {
     }
 }
 
-export default function LWSymbols() {
+export default function LWSymbolsPage() {
     return (
         <body className={styles.main}>
             <Image src={Background} alt={""} className={styles.background}/>
             <div className={styles.wrapper}>
                 <ImageHeader title={"Last Wish"} subtitle={"Symbols"} icon={BungieIcons.raids.last_wish} href={"chaosd2.com/lw/symbols"}/>
-                <div className={styles.content}>
-                    <ImageUnderConstruction/>
+                <div className={custom.content}>
+                    <div className={custom.table}>
+                        <Symbol image={LWSymbols.eight_snake} name={"Eight Snake"}/>
+                        <Symbol image={LWSymbols.infinity_snake} name={"Infinity Snake"}/>
+                        <Symbol image={LWSymbols.two_snake} name={"Two Snake"}/>
+                        <Symbol image={LWSymbols.uwu_snake} name={"Uwu Snake"}/>
+                        <Symbol image={LWSymbols.circle_fish} name={"Circle Fish"}/>
+                        <Symbol image={LWSymbols.curl_fish} name={"Curl Fish"}/>
+                        <Symbol image={LWSymbols.up_fish} name={"Up Fish"}/>
+                        <Symbol image={LWSymbols.left_fish} name={"Left Fish"}/>
+                        <Symbol image={LWSymbols.fire_left_dragon} name={"Fire Left Dragon"}/>
+                        <Symbol image={LWSymbols.fire_right_dragon} name={"Fire Right Dragon"}/>
+                        <Symbol image={LWSymbols.spear_hold_dragon} name={"Spear Hold Dragon"}/>
+                        <Symbol image={LWSymbols.spear_neck_dragon} name={"Spear Neck Dragon"}/>
+                        <Symbol image={LWSymbols.branch_bird} name={"Branch Bird"}/>
+                        <Symbol image={LWSymbols.down_bird} name={"Down Bird"}/>
+                        <Symbol image={LWSymbols.flying_bird} name={"Flying Bird"}/>
+                        <Symbol image={LWSymbols.left_bird} name={"Left Bird"}/>
+                    </div>
                 </div>
             </div>
         </body>
+    );
+}
+
+function Symbol({ image, name }){
+    return (
+        <div className={custom.symbol}>
+            <Image src={image} alt={name} width={120} height={120}/>
+            <div className={custom.symbol_name}>{name}</div>
+        </div>
     );
 }
---
+++ src/image_components/LWSymbols.js
@@ -0,0 +1,35 @@
+import Branch_Bird from "@images/icons/last_wish/branch_bird.png";
+import Circle_Fish from "@images/icons/last_wish/circle_fish.png";
+import Curl_Fish from "@images/icons/last_wish/curl_fish.png";
+import Down_Bird from "@images/icons/last_wish/down_bird.png";
+import Eight_Snake from "@images/icons/last_wish/eight_snake.png";
+import Fire_Left_Dragon from "@images/icons/last_wish/fire_left_dragon.png";
+import Fire_Right_Dragon from "@images/icons/last_wish/fire_right_dragon.png";
+import Flying_Bird from "@images/icons/last_wish/flying_bird.png";
+import Infinity_Snake from "@images/icons/last_wish/infinity_snake.png";
+import Left_Bird from "@images/icons/last_wish/left_bird.png";
+import Left_Fish from "@images/icons/last_wish/left_fish.png";
+import Spear_Hold_Dragon from "@images/icons/last_wish/spear_hold_dragon.png";
+import Spear_Neck_Dragon from "@images/icons/last_wish/spear_neck_dragon.png";
+import Two_Snake from "@images/icons/last_wish/two_snake.png";
+import Up_Fish from "@images/icons/last_wish/up_fish.png";
+import Uwu_Snake from "@images/icons/last_wish/uwu_snake.png";
+
+export const LWSymbols = {
+    branch_bird: Branch_Bird,
+    circle_fish: Circle_Fish,
+    curl_fish: Curl_Fish,
+    down_bird: Down_Bird,
+    eight_snake: Eight_Snake,
+    fire_left_dragon: Fire_Left_Dragon,
+    fire_right_dragon: Fire_Right_Dragon,
+    flying_bird: Flying_Bird,
+    infinity_snake: Infinity_Snake,
+    left_bird: Left_Bird,
+    left_fish: Left_Fish,
+    spear_hold_dragon: Spear_Hold_Dragon,
+    spear_neck_dragon: Spear_Neck_Dragon,
+    two_snake: Two_Snake,
+    up_fish: Up_Fish,
+    uwu_snake: Uwu_Snake,
+}
