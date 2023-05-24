d46a6647511046b0516f11c0fef92e2b15d9e5f7
ChaosCantrip <dev@chaoscantrip.com>
2023-05-24 13:45:40 +0100

    Updated home page

--- src/app/(website)/(home)/page.js
+++ src/app/(website)/(home)/page.js
@@ -15,7 +15,14 @@ export default function HomePage(){
             <div className={layout.page}>
                 <Title title={"Home"} show_back_button={false}/>
                 <div className={layout.page_content}>
-                    <p>Website very much under construction</p>
+                    <h2>Hi, welcome to chaosd2!</h2>
+                    <br/>
+                    <p>I wanted to create a place to share raid guides and images in an easier way to access.</p>
+                    <p>The whole website is built around the URLs being easy to use, such as just chaosd2.com/root/3 for Macrocosm, etc.</p>
+                    <p>All images are also packed with metadata so Discord displays them nicely!</p>
+                    <br/>
+                    <p>Since the website is so new, I'm still working on making a lot of the images. I'll get around to them at some point!</p>
+                    <p>Thanks for dropping by!</p>
                 </div>
             </div>
         </div>
