9d4fb628acb910c641eec84225f7cadc7d4d2d93
ChaosCantrip <dev@chaoscantrip.com>
2023-05-25 18:45:12 +0100

    Added voting placeholders

--- src/app/(website)/voting/page.js
+++ src/app/(website)/voting/page.js
@@ -1,43 +1,61 @@
-"use client";
-
 import layout from "@styles/modules/layout.module.css";
-import Voting from "@src/app/(website)/voting/voting";
-import VotingResults from "@src/app/(website)/voting/results";
-import BackgroundImage from "@components/BackgroundImage";
-import Background from "@backgrounds/home.jpg";
 import Title from "@components/Title";
+import {backgrounds} from "@components/ImageImports";
+import BackgroundImage from "@components/BackgroundImage";
 import {BungieIcons} from "@components/BungieIcons";
-import {useState} from "react";
 
-export const dynamic = true;
-
-export default function VotingPageWrapper() {
-    return (
-        <VotingPage/>
-    )
-}
-
-async function VotingPage() {
-    const [state, set_state] = useState(0);
-
+export default function VotingDisabled() {
     return (
         <div className={layout.page_wrapper}>
-            <BackgroundImage src={Background}/>
+            <BackgroundImage src={backgrounds.home}/>
             <div className={layout.page}>
-                <Title title={(state === 1) ? "Current Votes" : "Voting"} icon_url={BungieIcons.social_icon} show_back_button={false}/>
-                {(state === 0) ? <Voting add_vote={add_vote}/> : null}
-                {(state === 1) ? <VotingResults/> : null}
+                <Title title={"Voting is Currently Disabled"} icon_url={BungieIcons.social_icon} show_back_button={false}/>
+                <p>Voting has been disabled until I fix the backend. Please bear with me!</p>
             </div>
         </div>
     )
-
-    async function add_vote(ql) {
-        set_state(1);
-        await fetch("/api/vote", {
-            method: "POST",
-            body: JSON.stringify({
-                ql: ql
-            })
-        });
-    }
 }
+
+// "use client";
+//
+// import layout from "@styles/modules/layout.module.css";
+// import Voting from "@src/app/(website)/voting/voting";
+// import VotingResults from "@src/app/(website)/voting/results";
+// import BackgroundImage from "@components/BackgroundImage";
+// import Background from "@backgrounds/home.jpg";
+// import Title from "@components/Title";
+// import {BungieIcons} from "@components/BungieIcons";
+// import {useState} from "react";
+//
+// export const dynamic = true;
+//
+// export default function VotingPageWrapper() {
+//     return (
+//         <VotingPage/>
+//     )
+// }
+//
+// async function VotingPage() {
+//     const [state, set_state] = useState(0);
+//
+//     return (
+//         <div className={layout.page_wrapper}>
+//             <BackgroundImage src={Background}/>
+//             <div className={layout.page}>
+//                 <Title title={(state === 1) ? "Current Votes" : "Voting"} icon_url={BungieIcons.social_icon} show_back_button={false}/>
+//                 {(state === 0) ? <Voting add_vote={add_vote}/> : null}
+//                 {(state === 1) ? <VotingResults/> : null}
+//             </div>
+//         </div>
+//     )
+//
+//     async function add_vote(ql) {
+//         set_state(1);
+//         await fetch("/api/vote", {
+//             method: "POST",
+//             body: JSON.stringify({
+//                 ql: ql
+//             })
+//         });
+//     }
+// }
--- src/app/api/votes/route.js
+++ src/app/api/votes/route.js
@@ -1,7 +1,13 @@
-import {get_votes} from "@lib/db/voting";
 import {NextResponse} from "next/server";
 
 export async function GET(request) {
-    const votes = await get_votes();
-    return NextResponse.json(votes);
+    return NextResponse.json({message: "Voting is currently disabled"}, {status: 400});
 }
+
+// import {get_votes} from "@lib/db/voting";
+// import {NextResponse} from "next/server";
+//
+// export async function GET(request) {
+//     const votes = await get_votes();
+//     return NextResponse.json(votes);
+// }
