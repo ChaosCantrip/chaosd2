178ff1fdedd6f01436d20c6b2f06f10168479d7f
ChaosCantrip <dev@chaoscantrip.com>
2023-05-26 07:48:54 +0100

    Updated footer central message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import styles from "@styles/modules/footer.module.css";
+import WIPTag from "@components/WIPTag";
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <p>Website created by <a className={styles.link} href="https://jdonald.co.uk">ChaosCantrip</a></p>
-            <p>Not all images are my own, images by other people will be credited.</p>
+            <p>Website is very new, most guides are still a <span className={styles.wip}><WIPTag/></span></p>
             <p><a className={styles.link} href={"/legal"}>The Legal Stuff</a></p>
         </footer>
     )
