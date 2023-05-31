e053e8af7b54662c234149fbc386fc04f6e404bb
ChaosCantrip <dev@chaoscantrip.com>
2023-05-31 11:07:24 +0100

    Fixed WIP tag in footer throwing errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 import styles from "@styles/modules/footer.module.css";
-import WIPTag from "@components/WIPTag";
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <p>Website created by <a className={styles.link} href="https://jdonald.co.uk">ChaosCantrip</a></p>
-            <p className={styles.desktop_only}>Website is very new, most guides are still a <span className={styles.wip}><WIPTag/></span></p>
+            <p className={styles.desktop_only}>Website is very new, most guides are still WIP</p>
             <p><a className={styles.link} href={"/legal"}>The Legal Stuff</a></p>
         </footer>
     )
