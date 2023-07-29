c8cbd802b6d0645872947a384fb3b4140d6bdf8f
ChaosCantrip <dev@chaoscantrip.com>
2023-07-29 21:52:18 +0100

    Revert "Added thank_you test endpoint"
    This reverts commit de03b25df04ddcfef6495c1314356c8c3fa933ba.

--- src/app/api/thank_you/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(request) {
-    const body = await request.json();
-    console.log(body);
-    return NextResponse.json({message: "Success!"});
-}
