c156e1058add98ed80a2cc903959e47a178c0481
ChaosCantrip <dev@chaoscantrip.com>
2023-09-21 17:04:04 +0100

    Added generate_unknown_item

--- src/lib/bungie/APIHandler.js
+++ src/lib/bungie/APIHandler.js
@@ -196,12 +196,35 @@ const static_definitions = {
     },
 }
 
+function generate_unknown_item(item_hash) {
+    return {
+        "displayProperties": {
+            "name": "Unknown Item",
+            "icon": "https://www.bungie.net/common/destiny2_content/icons/ef1a8374c931321694d6d3d0d3e96247.png",
+        },
+        "inventory": {
+            "recipeItemHash": 0
+        },
+        "equippingBlock": {
+            "ammoType": 1
+        },
+        "damageTypeHashes": [
+            damage.Strand
+        ],
+        "itemTypeAndTierDisplayName": `Unknown Item (${item_hash})`,
+    }
+}
+
 export async function get_definition(definition_type, definition_hash) {
     const response = await fetch(`https://www.bungie.net/Platform/Destiny2/Manifest/${definition_type}/${definition_hash}`, {
         headers: {
             "X-API-Key": process.env.BUNGIE_API_KEY
         }
     });
+    if (response.status !== 200) {
+        console.log(`Error fetching definition for ${definition_hash}`);
+        return undefined;
+    }
     const data = await response.json();
     return data["Response"];
 }
@@ -212,7 +235,12 @@ export async function get_item_definition(item_hash) {
         return static_definitions[item_hash];
     } else {
         console.log(`Fetching definition for ${item_hash}`);
-        return await get_definition("DestinyInventoryItemDefinition", item_hash);
+        const definition = await get_definition("DestinyInventoryItemDefinition", item_hash);
+        if (definition === undefined) {
+            return generate_unknown_item(item_hash);
+        } else {
+            return definition;
+        }
     }
 }
 
