0d10560f3259923b694f9475687190b563b0c6c5
ChaosCantrip <dev@chaoscantrip.com>
2023-12-05 12:00:20 +0000

    Added screenshots wildcards

--- tests/screenshots.spec.js
+++ tests/screenshots.spec.js
@@ -25,6 +25,33 @@ test("screenshot", async ({ page }) => {
     }
     console.log(list_of_paths);
 
+    const to_add = [];
+    const to_remove = [];
+
+    for (let i = 0; i < list_of_paths.length; i++) {
+        const given_path = list_of_paths[i];
+        if (given_path.endsWith("*")) {
+            const path = given_path.split("*")[0];
+            const files = fs.readdirSync(`src/app/image_sources/${path}`);
+            for (let j = 0; j < files.length; j++) {
+                to_add.push(`${path}${files[j]}`);
+            }
+            to_remove.push(given_path);
+        }
+    }
+
+    for (let i = 0; i < to_remove.length; i++) {
+        list_of_paths.splice(list_of_paths.indexOf(to_remove[i]), 1);
+    }
+
+    for (let i = 0; i < to_add.length; i++) {
+        list_of_paths.push(to_add[i]);
+    }
+
+    if (to_add.length > 0) {
+        console.log(list_of_paths);
+    }
+
     for (let i = 0; i < list_of_paths.length; i++) {
         const given_path = list_of_paths[i];
         await page.goto(`http://localhost:3000/image_sources/${given_path}`);
