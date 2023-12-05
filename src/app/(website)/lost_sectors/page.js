a1340f352960a154664bb76f738dde6872123c1b
ChaosCantrip <dev@chaoscantrip.com>
2023-12-05 12:20:13 +0000

    Added main lost sectors page

--- src/app/(website)/lost_sectors/page.js
+++ src/app/(website)/lost_sectors/page.js
@@ -3,6 +3,13 @@ import BackgroundImage from "@components/BackgroundImage";
 import Background from "@backgrounds/raid.jpg";
 import Title from "@components/Title";
 import {BungieIcons} from "@components/BungieIcons";
+import Image from "next/image";
+import Dropdown from "@components/Dropdown";
+import custom from "@src/app/(website)/[rad]/[activity]/custom.module.css";
+import QuickLink from "@components/QuickLink";
+import Link from "next/link";
+import {image_sources} from "@components/ImageImports";
+import {PathConfig} from "@components/PathConfig";
 
 export const metadata = {
     title: "Lost Sectors",
@@ -26,7 +33,21 @@ export default function LostSectorsPage() {
             <div className={layout.page}>
                 <Title title={"Lost Sectors"} icon_url={BungieIcons.lost_sectors_icon} show_back_button={false}/>
                 <div className={layout.page_content}>
-                    <p>Page Under Construction</p>
+                    {Object.keys(PathConfig.lost_sectors).map((lost_sector_id) => {
+                        const lost_sector = PathConfig.lost_sectors[lost_sector_id];
+                        return (
+                            <Dropdown title={`${lost_sector.name}, ${lost_sector.location}`} href={lost_sector.ql} key={lost_sector_id} done={true}>
+                                <div className={custom.dropdown_content}>
+                                    <QuickLink href={lost_sector.ql}/>
+                                    <div className={custom.dropdown_image_wrapper}>
+                                        <Link href={`${lost_sector.ql}/i`} className={custom.image_link}>
+                                            <Image src={image_sources.lost_sectors[lost_sector_id]} alt={<a href={lost_sector.ql}>{lost_sector.name}</a>} className={custom.dropdown_image} placeholder="blur"/>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </Dropdown>
+                        )
+                    })}
                 </div>
             </div>
         </div>
