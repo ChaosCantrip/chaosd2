cf064f6e0de553895988c4108e304f8294f31ba8
ChaosCantrip <dev@chaoscantrip.com>
2024-03-23 08:57:41 +0000

    Changed KeepAlive text

--- src/image_components/KeepAlive.js
+++ src/image_components/KeepAlive.js
@@ -4,7 +4,7 @@ export default function KeepAlive() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.text_wrapper}>
-                <p className={styles.text}><span>chaosd2</span> needs your help!</p>
+                <p className={styles.text}><span>chaosd2</span> might be closing down!</p>
                 <p className={styles.text}>Learn more and help keep us online at <span>chaosd2.com/support</span></p>
             </div>
         </div>
