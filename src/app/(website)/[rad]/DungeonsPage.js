abf92f0d0f08ad0dee3c034c89fb42ed42b47cc5
ChaosCantrip <dev@chaoscantrip.com>
2024-05-22 16:03:34 +0100

    Added Warlord's Ruin to the dungeons page

--- src/app/(website)/[rad]/DungeonsPage.js
+++ src/app/(website)/[rad]/DungeonsPage.js
@@ -31,6 +31,7 @@ export default function DungeonsPage() {
                 <Title title={"Dungeons"} icon_url={BungieIcons.dungeon_icon} show_back_button={false}/>
                 <div className={layout.page_content}>
                     <div className={layout.activity_grid}>
+                        <ActivityGridItem href={"/dungeons/warlords_ruin"} name={"Warlord's Ruin"} icon={BungieIcons.dungeons.warlords_ruin} banner={GhostsOfTheDeep} description={"A blighted vengeance festers deep in the mountains of the EDZ."}/>
                         <ActivityGridItem href={"/dungeons/ghosts_of_the_deep"} name={"Ghosts of the Deep"} icon={BungieIcons.dungeons.ghosts_of_the_deep} banner={GhostsOfTheDeep} description={"Drown in the deep, or rise from it."}/>
                         <ActivityGridItem href={"/dungeons/spire_of_the_watcher"} name={"Spire of the Watcher"} icon={BungieIcons.dungeons.spire_of_the_watcher} banner={SpireOfTheWatcher} description={"Machinations run wild in this dust-ridden ruin. Bring them to heel."}/>
                         <ActivityGridItem href={"/dungeons/duality"} name={"Duality"} icon={BungieIcons.dungeons.duality} banner={Duality} description={"Dive into the depths of the exiled emperor's mind in search of dark secrets."}/>
